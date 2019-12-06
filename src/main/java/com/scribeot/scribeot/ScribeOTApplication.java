343fe71cce2d2e8b0a01809c5c011ddf462e214e
Miles <mileshenrichs21@gmail.com>
2019-12-06 14:44:08 -0600

    initialize Spring application

---
+++ src/main/java/com/scribeot/scribeot/ScribeOTApplication.java
@@ -0,0 +1,13 @@
+package com.scribeot.scribeot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribeOTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScribeOTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scribeot/scribeot/ScribeOTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scribeot.scribeot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribeOTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
