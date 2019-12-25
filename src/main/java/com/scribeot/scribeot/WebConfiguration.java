375d12148de6f9c3b187d0f7d72b01f022678e1a
Miles <mileshenrichs21@gmail.com>
2019-12-25 14:10:32 -0600

    add web configuration to redirect all requests to single page app

---
+++ src/main/java/com/scribeot/scribeot/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.scribeot.scribeot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * This configuration solves the single-page app routing problem.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/");
+    }
+}
